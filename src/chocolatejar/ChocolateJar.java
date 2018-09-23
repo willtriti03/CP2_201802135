96078afa3d344c5f806bba9e9acbaa4c593fdaf0
jungjune <willtriti03@naver.com>
2018-09-23 23:50:45 +0900

    Main

--- src/chocolatejar/ChocolateJar.java
+++ src/chocolatejar/ChocolateJar.java
@@ -1,37 +1,37 @@
 package chocolatejar;
 
-public class ChocolateJar implements Jar{
+public class ChocolateJar implements Jar{	
 	
-	private int chocolate;
-	private int chilli;
+	private int chocolate;						//초콜릿 개수 변수
+	private int chilli;							//칠리 개수 변수
 	
-	public  ChocolateJar() {
+	public  ChocolateJar() {					//초콜릿 항아리 생성자 생성시 디폴트 값으로 초콜릿과 칠리 생성
 		chocolate = 13;
 		chilli = 1;
 	}
 	
-	public  ChocolateJar(int chocolate) {
+	public  ChocolateJar(int chocolate) {		//인자값으로 초콜릿 개수가 들어온다면 초콜릿 개수를 설정하여 생성
 		this.chocolate = chocolate;
 		chilli = 1;
 	}
 
 
 	@Override
-	public int getItem() {
+	public int getItem() {						//항아리내 모든 아이템의 개수 반환
 		return chocolate + chilli;
 	}
 	
-	public int getChocolate() {
+	public int getChocolate() {					//초콜릿 개수 반환
 		return chocolate;
 	}
 	
-	public int getChilli() {
+	public int getChilli() {					//칠리 개수 반환
 		return chilli;
 	}
 	
 	@Override
-	public void takeItem(int hand) {
-		if(getItem() - hand < 0) {
+	public void takeItem(int hand) {			//hand만큼 아이템을 뽑음
+		if(getItem() - hand < 0) {				//항아리내 아이템보다 많이 뽑으려한다면 경고 메세지 출력
 			System.out.println("남은 개수보다 많습니다");
 		}
 		else {
